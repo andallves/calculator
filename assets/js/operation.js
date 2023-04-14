7cc9936e6c1c6b08884acb79f12801c194241c57
Andre Alves <37510133+andallves@users.noreply.github.com>
2023-04-14 19:43:38 -0300

    change to constructor function

--- assets/js/operation.js
+++ assets/js/operation.js
@@ -1,64 +1,48 @@
-function createCalculator() {
-  return {
-    display: document.querySelector('.display'),
+function Calculator() {
+  this.display = document.querySelector('.display');
 
-    start() {
-      this.buttonClick();
-      this.pressionEnter();
-    },
+  this.start() => {
+    this.buttonClick();
+    this.pressionEnter();
+  };
 
-    pressionEnter() {
-      this.display.addEventListener('keypress', e => {
-        console.log(e);
-        if (e.keyCode === 13) this.doOperation();
-      });
-    },
+  this.pressionEnter() => {
+    this.display.addEventListener('keyup', e => {
+      if (e.key === 'Enter') this.doOperation();
+    });
+  };
 
-    doOperation() {
-      let operation = this.display.value;
+  this.buttonClick() => {
+    document.addEventListener('click', e => {
+      const element = e.target;
 
-      try {
-        operation = eval(operation);
+      if (element.classList.contains('num')) this.btnToDisplay(element.innerText);
+      if (element.classList.contains('clear')) this.clearDisplay();
+      if (element.classList.contains('delete')) this.deleteDisplay();
+      if (element.classList.contains('equal')) this.doOperation();
+    });
+  };
 
-        if (!operation || (Number.isNaN(operation))) return;
+  this.doOperation = () => {
+    try {
 
-        this.display.value = operation;
+      let operation = eval(this.display.value);
+      if (!operation) return;
+      this.display.value = operation;
 
-      } catch (e) {
-        alert('Conta inválida!')
-        return;
-      }
-    },
-
-    clearDisplay() {
-      this.display.value = '';
-    },
-
-    deleteDisplay() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-
-
-    buttonClick() {
-      document.addEventListener('click', e => {
-        const element = e.target;
-
-        if (element.classList.contains('num')) this.btnToDisplay(element.innerText);
-
-        if (element.classList.contains('clear')) this.clearDisplay();
-
-        if (element.classList.contains('delete')) this.deleteDisplay();
-
-        if (element.classList.contains('equal')) this.doOperation();
-      });
-    },
-
-    btnToDisplay(value) {
-      this.display.value += value;
+    } catch (e) {
+      alert('Conta inválida!')
+      return;
     }
-  }
+  };
 
+  this.btnToDisplay = element => {
+    this.display.value += elemnt;
+    this.display.focus();
+  };
+  this.clearDisplay = () => this.display.value = '';
+  this.deleteDisplay = () => this.display.value = this.display.value.slice(0, -1);
 }
 
-const calculator = createCalculator();
+const calculator = new Calculator();
 calculator.start();
